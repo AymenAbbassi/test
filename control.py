de172fc3f1074724abd4be2498baa115e19e53bb
Aymen Abbassi <AymenNA2026@bridge-rayn.org>
2024-02-25 11:52:23 -0500

    further changes

--- control.py
+++ control.py
@@ -1,29 +1,30 @@
 from simple_pid import PID
-import os
-import yaml
+from yaml import safe_load
+from time import sleep
+from xbox_controller import XboxController
 
 class PIDController:
 
     def __init__(self, name: str) -> None:
         self.name = name
-        self.yaml_file_configs = self.get_yaml_file()
         self.pid_controller = PID()
-        self.pid_controller.Kp = self.yaml_file_configs[0], 
-        self.pid_controller.Ki = self.yaml_file_configs[1], 
-        self.pid_controller.Kd = self.yaml_file_configs[2],
-        self.pid_controller.setpoint = self.yaml_file_configs[3]
-        self.pid_controller.sample_time = self.yaml_file_configs[4]
+        self.pid_controller.Kp = self.get_yaml_file[0] 
+        self.pid_controller.Ki = self.get_yaml_file[1] 
+        self.pid_controller.Kd = self.get_yaml_file[2]
+        self.pid_controller.setpoint = self.get_yaml_file[3]
+        self.pid_controller.sample_time = self.get_yaml_file[4]
           
     def __call__(self, input: float) -> float:
         """Return the output of the PID controller"""
+        #print(f"KP: {self.pid_controller.Kp} KI: {self.pid_controller.Ki}, KD: {self.pid_controller.Kd}, Update: {self.pid_controller.sample_time}, Setpoint: {self.pid_controller.setpoint}")
         return self.pid_controller.__call__(input)
 
-
+    @property
     def get_yaml_file(self) -> list[float, float, float, float] | None:
         """Get the yaml file configuration for this PID controller"""
         try:
             with open(r"pid\pid_configs.yaml", "r") as f:
-                yaml_file = yaml.safe_load(f)
+                yaml_file = safe_load(f)
             return [yaml_file[f"{self.name}"]["KP"], yaml_file[f"{self.name}"]["KI"], yaml_file[f"{self.name}"]["KD"], yaml_file[f"{self.name}"]["Setpoint"], yaml_file[f"{self.name}"]["Update_Rate"]]
         except KeyError:
             print("The wrong key was entered")
@@ -39,6 +40,10 @@ class PIDController:
 
 def main() -> None:
     pid1 = PIDController("PID1")
+    xbox_controller = XboxController(0.2)
+    while True:
+        print(pid1(xbox_controller.joysticks[0]))
+        sleep(0.1)
 
 if __name__ == "__main__":
     main()
