5345065470cdc0957ddec2d1193e67ced74c87bd
Aymen Abbassi <AymenNA2026@bridge-rayn.org>
2024-03-07 16:27:59 -0500

    logging changes

--- logger.py
+++ logger.py
@@ -2,38 +2,33 @@ from datetime import datetime
 from random import randint
 from time import sleep
 from typing import Dict
+import enum
+
+class Colors:
+    ERROR = "\x1B[38;5;124m"
+    FATAL_ERROR ="\x1B[38;5;52m"
+    WARNING = "\x1B[38;5;172m"
+    GOOD = "\x1B[38;5;46m"
+    CLEAR = "\033[0m"
 
 def log(message: str, *args) -> None:
     time: datetime = datetime.now()
     for arg in args:
         match arg:
             case "warn":
-                print(f"\033[93m [{time}] [WARN] {message}\033[93m")
+                print(f"{Colors.CLEAR}{Colors.WARNING}[{time}] [WARN] {message}{Colors.WARNING}{Colors.CLEAR}")
             case "err":
-                print(f"\033[91m [{time}] [ERR] {message}\033[91m")
+                print(f"{Colors.CLEAR}{Colors.ERROR}[{time}] [ERR] {message}{Colors.ERROR}{Colors.CLEAR}")
             case "std":
-                print(f"\033[0m [{time}] {message} \033[0m")
+                print(f"{Colors.CLEAR}[{time}] {message}{Colors.CLEAR}")
+            case "fatal":
+                print(f"{Colors.CLEAR}{Colors.FATAL_ERROR}[{time}] [FATAL] {message}{Colors.FATAL_ERROR}{Colors.CLEAR}")
             case "good":
-                print(f"\033[0m\033[92m [{time}] {message} \033[92m\033[0m")
+                print(f"{Colors.CLEAR}{Colors.GOOD}[{time}] {message}{Colors.GOOD}{Colors.CLEAR}")
             case "general":
-                print(f"\033[1m\033[0m\033[4m [{time}] {message} \033[4m\033[1m\033[0m")
+                print(f"{Colors.CLEAR}[{time}] {message}{Colors.CLEAR}")
             case _:
                 raise ValueError("\033[91m An invalid type was entered \033[91m \n")
 
-if __name__ == "__main__":
-    while True:
-        random_number: int = randint(1, 5)
-        match random_number:
-            case 1:
-                log("hello", "warning")
-            case 2:
-                log("another message", "error")
-            case 3:
-                log("another file", "standard")
-            case 4:
-                log("This is a good message", "good")
-            case 5:
-                log("This is a general message", "general")
-        sleep(0.1)
 
 
